0cf92fdacba2695bba6e343bde48787f3d1a64ab
Bearseerk <0324108232@ut-tijuana.edu.mx>
2026-08-06 21:32:54 -0700

    Conecta recetas a la base de datos

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import DestilacionDashboard from "./DestilacionDashboard";
 
 function App() {
   const [pantalla, setPantalla] = useState("login");
+  const [usuario, setUsuario] = useState(null);
 
   if (pantalla === "dashboard") {
-    return <DestilacionDashboard onLogout={() => setPantalla("login")} />;
+    return (
+      <DestilacionDashboard
+        usuario={usuario}
+        onLogout={() => {
+          setUsuario(null);
+          setPantalla("login");
+        }}
+      />
+    );
   }
 
-  return <LoginDestilacionNuevo onLogin={() => setPantalla("dashboard")} />;
+  return (
+    <LoginDestilacionNuevo
+      onLogin={(user) => {
+        setUsuario(user);
+        setPantalla("dashboard");
+      }}
+    />
+  );
 }
 
 export default App;
